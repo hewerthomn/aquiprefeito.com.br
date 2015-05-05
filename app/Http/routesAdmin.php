ad188ba2f0face21042782fb8b1b1f851b44e2a7
Éverton Inocêncio <hewerthomn@gmail.com>
2015-05-05 00:50:04 -0400

    adicionado rotas de editar perfil e trocar senha

--- app/Http/routesAdmin.php
+++ app/Http/routesAdmin.php
@@ -7,5 +7,6 @@ Route::group(['prefix' => 'admin'], function() {
 
 	Route::get('/', ['as' => 'admin.dashboard', 'uses' => 'Admin\AdminController@dashboard']);
 	Route::get('profile', ['as' => 'admin.profile', 'uses' => 'Admin\AdminController@profile']);
-
+	Route::post('profile', ['as' => 'admin.profile.edit', 'uses' => 'Admin\AdminController@profileUpdate']);
+	Route::post('changePassword', ['as' => 'admin.changePassword', 'uses' => 'Admin\AdminController@changePassword']);
 });
