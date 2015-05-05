8cd8eddcacf0c3689e5ea036db56e19599063f29
Éverton Inocêncio <hewerthomn@gmail.com>
2015-05-05 00:37:12 -0400

    adicionado propriedade redirectTo

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -20,6 +20,8 @@ class AuthController extends Controller {
 
 	use AuthenticatesAndRegistersUsers;
 
+	protected $redirectTo = '/admin';
+
 	/**
 	 * Create a new authentication controller instance.
 	 *
