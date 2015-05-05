3e722e58f1e922bce8b9d40507c4b2b117b5f477
Éverton Inocêncio <hewerthomn@gmail.com>
2015-05-05 00:44:21 -0400

    criado view de dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<h1>Administração</h1>
+<hr>
+
+<div class="row">
+	<div class="col-md-6">
+
+	</div>
+	<div class="col-md-6">
+
+	</div>
+</div>
+
+@stop
